a5fae5ae31bfb0ab325186d5667ee134a519711d
banpresto <sexyteemo@gmail.com>
2020-12-06 21:11:27 +0300

    lesson_2

--- db/migrate/20201204185114_add_correct_to_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddCorrectToAnswers < ActiveRecord::Migration[6.0]
-  def change
-    change_column :answers, :correct, :boolean, default: false
-  end
-end
--- db/migrate/20201206132823_add_question_ref_to_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionRefToAnswers < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :answers, :question, null: false, foreign_key: true
-  end
-end
