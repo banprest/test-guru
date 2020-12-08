f8f536d0154faa28dcf914ef236847de9b371d77
banpresto <sexyteemo@gmail.com>
2020-12-08 23:47:06 +0300

    lesson_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
   def self.test_and_its_category(title)
-    joins('JOIN categories ON category_id = categories.id').where("categories.title = ? ", title).order('id DESC')
+    select(:title).joins('JOIN categories ON category_id = categories.id').where("categories.title = ? ", title).order('tests.id DESC')
   end
 end
--- db/migrate/20201208105923_create_user_infos.rb
+++ db/migrate/20201208105923_create_user_infos.rb
@@ -3,6 +3,9 @@ class CreateUserInfos < ActiveRecord::Migration[6.0]
     create_table :user_infos do |t|
 
       t.timestamps
+      #add_reference :user_infos, :test, foreign_key: true
+      #add_reference :user_infos, :user, foreign_key: true
+      #Выдает ошибку что такой таблицы нет SQLite3::SQLException: no such table: user_infos
     end
     add_reference :user_infos, :test, foreign_key: true
     add_reference :user_infos, :user, foreign_key: true
