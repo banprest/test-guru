27f64012827a6a5cb15cdf505a40e27f1cb7c2d9
banpresto <sexyteemo@gmail.com>
2021-01-10 20:58:49 +0300

    lesson_10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,12 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
-
-  helper_method :current_user,
-                :logged_in?,
-                :log_out
-  private
-
-  def authenticate_user!
-    unless current_user
-      cookies[:return_original_url] = request.original_url
-      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
-    end
+  def after_sign_in_path_for(resource)
+    if current_user.is_a?(Admin)
+      flash[:notice] = "Hello! #{current_user.first_name} #{current_user.last_name}"
+      admin_tests_path
+    else
+      tests_path
+    end    
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
-
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
-
-  MAIL = /[a-z0-9]@[a-z]+\.[a-z]+/i
+  
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test'
-
-  validates :mail, presence: true, format: MAIL, uniqueness: true
-
-  has_secure_password
 
   def pass_test(level)
     tests.where(level: level)
