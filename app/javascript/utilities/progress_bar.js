6f005db82728973238432eb0b413a1d20c88ed83
banpresto <sexyteemo@gmail.com>
2021-01-25 21:44:18 +0300

    lesson 16

--- app/javascript/utilities/progress_bar.js
+++ app/javascript/utilities/progress_bar.js
@@ -2,7 +2,7 @@ document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.progress-bar')
 
   if (control) {
-  var progress = document.querySelector('.progress-bar').dataset.progress
-  document.getElementById('progress').value = progress
+    var progress = document.querySelector('.progress-bar').dataset.progress
+    document.getElementById('progress').value = progress - 1
   }
 })
