4dd9a475898893b5bfb400630474aa31a784a0f4
banpresto <sexyteemo@gmail.com>
2021-01-28 19:04:42 +0300

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'calm-sands-31142.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'banpresto-test-guru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
